c007a991dc2b09c17c31f96be7e779958d6f9f3b
Ivan Tushynski <tushinski.ivan@gmail.com>
2022-10-24 00:48:50 +0300

    update

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -2,6 +2,38 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //вывод в консоль
+        //Console.WriteLine("Hello, World!");
+        //Console.ReadKey();
+
+        testRandom();
+
+        int a = 1;
+
+        if (a == 1)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                a = a + 1;
+                Console.WriteLine(a);
+                Thread.Sleep(1000);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Dont Working!!!");
+        }
+
+    }
+
+    static void testRandom()
+    {
+        var rand = new Random();
+        var bytes = new byte[5];
+        rand.NextBytes(bytes);
+        Console.WriteLine("Five random byte values:");
+        foreach (byte byteValue in bytes)
+            Console.Write("{0, 5}", byteValue);
+        Console.WriteLine();
     }
 }
