8182989c98d2a8de34a66a99daf5123f993338b4
Ivan Tushynski <tushinski.ivan@gmail.com>
2022-10-25 01:52:47 +0300

    update

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -7,13 +7,15 @@
         //Console.ReadKey();
 
         testRandom();
+        DataVariables dataVariables = new DataVariables();
+        dataVariables.OneTwo();
 
         int a = 1;
 
 
         if (a == 1)
         {
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 5; i++)
             {
                 a = a + 1;
                 Console.WriteLine(a);
@@ -26,22 +28,33 @@
         }
 
     }
-
-    static void testRandom()
-    {
-        var rand = new Random();
-        var bytes = new byte[5];
-        rand.NextBytes(bytes);
-        Console.WriteLine("Five random byte values:");
-        foreach (byte byteValue in bytes)
-            Console.Write("{0, 5}", byteValue);
-        Console.WriteLine();
-    }
 }
 
-class Human
+class DataVariables
 {
+    //string используется для вывода слов, фраз и любых других буквенно-цифровых данных, но не для выполнения вычислений с ними;
+    //char представляет отдельную букву или цифру;
+    //int представляет целое число;
+    //decimal представляет число с десятичной дробью;
+    //bool представляет значения "истина" или "ложь".
     string name = "Vova";
     int age = 10;
     double heigh = 174.5;
+    bool liam = true;
+    decimal liamTwo = 123.425m;
+
+
+    public void OneTwo()
+    {
+        Console.WriteLine(name + " string");
+        Console.WriteLine(age + " int");
+        Console.WriteLine(heigh + " double");
+        Console.WriteLine(liam + " bool");
+        Console.WriteLine(liamTwo + " decimal");
+        //ключевое слово var указывает компилятору на необходимость выведения типа данных переменной на основе значения, используемого при инициализации;
+        //Ключевое слово var можно встретить и в других примерах кода, но по возможности следует использовать фактический тип данных.
+        //вар используется только внутри методов или циклов!!!
+        var altima = 34;
+        Console.WriteLine(altima + " var");
+    }
 }
