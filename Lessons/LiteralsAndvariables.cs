1e9a645a068b8b31ffb0fcc5ed64284363faae16
Ivan Tushynski <tushinski.ivan@gmail.com>
2022-10-27 00:28:02 +0300

    update

--- Lessons/LiteralsAndvariables.cs
+++ Lessons/LiteralsAndvariables.cs
@@ -49,5 +49,31 @@
         //------------------------------------------------------
         int arm = 50 % 10;
         Console.WriteLine(arm + " ostatok ot deleniya");
+        //-------------------------------------------------------------------------------------------
+        int value1 = 3 + 4 * 5;
+        int value2 = (3 + 4) * 5;
+        Console.WriteLine(value1);
+        Console.WriteLine(value2);
+        //------------------------------------------------------------
+        int value = 1;
+
+        value = value + 1;
+        Console.WriteLine("First increment: " + value);
+
+        value += 1;
+        Console.WriteLine("Second increment: " + value);
+
+        value++;
+        Console.WriteLine("Third increment: " + value);
+
+        value = value - 1;
+        Console.WriteLine("First decrement: " + value);
+
+        value -= 1;
+        Console.WriteLine("Second decrement: " + value);
+
+        value--;
+        Console.WriteLine("Third decrement: " + value);
+        Console.WriteLine(5 / 10);
     }
 }
